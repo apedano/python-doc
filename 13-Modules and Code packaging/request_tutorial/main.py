8fb9925c2aae5e4bbd4f42497d6eb25436141579
Alessandro <pedano.alessandro@gmail.com>
2024-11-28 15:56:29 +0100

    WIP testing

--- 13-Modules and Code packaging/request_tutorial/main.py
+++ 13-Modules and Code packaging/request_tutorial/main.py
@@ -1,14 +1,5 @@
-# 13A Sample modules: Request
+import json
 
-## Request 
-
-`requests` is a package to interact with web services.
-
-Create a virtual env in the `request_tutorial` subfolder and activate it.
-
-### Get call
-
-```python
 import requests
 
 BASE_URL = 'https://fakestoreapi.com'
@@ -27,11 +18,7 @@ print(response.json())
 
 response = requests.get(f"{BASE_URL}/products/2")
 print(response.json())
-```
 
-### Post call
-
-```python
 import requests
 
 BASE_URL = 'https://fakestoreapi.com'
@@ -45,12 +32,8 @@ new_product = {
 }
 
 response = requests.post(f"{BASE_URL}/products", json=new_product)
-print(response.json()) # {'id': 21, 'title': 'test product', 'price': 13.5, 'description': 'lorem ipsum set', 'image': 'https://i.pravatar.cc', 'category': 'electronic'}
-```
+print(response.json()) #
 
-If we don't use the json argument, we have to make the POST request like this:
-
-```python
 new_product_json_string = json.dumps(new_product) #converts the object (dictionary) to json formatted string
 
 headers = {
@@ -59,4 +42,8 @@ headers = {
 
 response = requests.post(f"{BASE_URL}/products", data=json.dumps(new_product), headers=headers)
 print(response.json())
-```
+
+
+
+
+
---
+++ 14-Testing with Pytest/14-Testing.md
@@ -0,0 +1,7 @@
+# 14-Testing
+
+## PyTest
+https://betterstack.com/community/guides/testing/pytest-guide/
+https://semaphoreci.com/community/tutorials/testing-python-applications-with-pytest
+
+PyTest: https://docs.pytest.org/en/stable/
