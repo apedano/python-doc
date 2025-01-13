deb13f758e79bc18c3c85424616ec24c7155497c
apedano <pedano.alessandro@gmail.com>
2025-01-13 21:31:15 +0100

    WIP ISP classes (test to finalize)

---
+++ 15-SOLID principles/solid_demo/solid_demo/isp_classes.py
@@ -0,0 +1,56 @@
+import string
+from abc import abstractmethod, ABC
+
+
+# A change in one operation method need recompile also of the dependent classes which are not dependent
+# on that operation
+# On python dependencies are resolved in runtime so no recompile is needed
+# Interfaces Segregation Principle is related to those (statically typed )languages that require compilation
+class WrongIndependentService:
+    def operation1(self, service_name: string):
+        return "operation1 at {}".format(service_name)
+
+    def operation2(self, service_name: string):
+        return "operation1 at {}".format(service_name)
+
+    def operation3(self, service_name: string):
+        return "operation1 at {}".format(service_name)
+
+
+class WrongService1():
+    def __init__(self, independent_service: WrongIndependentService):
+        self.__service = independent_service
+
+    def do_operation(self):
+        self.__service.operation1("wrong service name")
+
+
+class Operations1(ABC):
+    @abstractmethod
+    def operation1(self, service_name: string):
+        pass
+
+class Operations2(ABC):
+    @abstractmethod
+    def operation2(self, service_name: string):
+        pass
+
+class Operations3(ABC):
+    @abstractmethod
+    def operation3(self, service_name: string):
+        pass
+
+class Service(Operations1, Operations2, Operations3):
+
+    def operation1(self, service_name: string):
+        return "operation1 at {}".format(service_name)
+
+    def operation2(self, service_name: string):
+        return "operation2 at {}".format(service_name)
+
+    def operation3(self, service_name: string):
+        return "operation3 at {}".format(service_name)
+
+
+
+
---
+++ 15-SOLID principles/solid_demo/solid_demo/tests/test_isp_classes.py
@@ -0,0 +1,5 @@
+from solid_demo.isp_classes import WrongService1
+
+
+def test_isp_violation():
+    wrong_service_1: WrongService1 = WrongService1()
